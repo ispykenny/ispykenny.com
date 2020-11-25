a00d53feb59a13ed89bbe8c033b54bb1d2a7845f
ispykenny <kenny.krosky@gmail.com>
2020-11-25 13:26:41 -0800

    font implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inner">
+        <h1 className="page-title">This is an h1</h1>
+        <h1>This is an h1</h1>
+        <h2>This is an h2</h2>
+        <h3>This is an h3</h3>
+        <h4>This is an h4</h4>
+        <h5>This is an h5</h5>
+        <h6>This is an h6</h6>
+        <p>lorem this is some body copy that I want to see what looks good. Let me know if you think it looks great and I'll continue to use it. yessss this is awesomeeeeee.   </p>
+      </div>
     </div>
   );
 }
