f71be045a993536184c81637a858941233f1a274
ispykenny <kenny.krosky@gmail.com>
2020-12-11 11:54:03 -0800

    removed ig

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Hero from '../components/Hero';
-import Instagram from '../components/Instagram';
-
 
 const About = () => {
   return (
@@ -11,7 +9,6 @@ const About = () => {
         copy="<p>I'm Kenny Krosky. A front end developer from Southern California 🌴. I'm a father of two girls, Kayden and Dakota and married to my best friend, Ramey. We own two dogs, two cats, and two fishes so we basically live on a farm at this point. I'm a huge tech and coffee nerd and love spending time with my family. I'm currently a full time dev at <strong>Adobe</strong> where I'm managing web initivates for my team.</p>"
         showSocial={true}
       />
-      <Instagram/>
     </div>
   )
 }
