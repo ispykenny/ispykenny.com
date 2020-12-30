34ee2ee8b1737967176d22b84a7ee20c1b53b347
ispykenny <kenny.krosky@gmail.com>
2020-12-30 10:01:39 -0800

    updates

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
     <Inner>
       <div style={{margin: 40}}>
       <form name="contact" netlify="true">
-      <input type="hidden" name="form-name" value="contact" />
+      <input type="hidden"method="POST" name="form-name" value="contact" />
           <p>
             <label>Your Name: <input type="text" name="name"/></label>
           </p>
