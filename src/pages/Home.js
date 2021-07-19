b24e5d5753990860e9dee4cfdbb21db9bfe012be
Kenny Krosky <kenny.krosky@gmail.com>
2021-07-19 08:12:35 -0700

    update about me

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
     <div className="home">
       <Hero
         src={Me}
-        content="<h1>Hello, I'm Kenny Krosky. I'm a Software Engineer and UX Designer.</h1><p>I'm a family man (2 kids and 1 on the way) and a big time coffee sipper. I'm passionate about all things Front-end, specifically JavaScript, CSS, and API related. Currently employed by Adobe, leading and maintaining the Creative Cloud growth web domain.</p>"
+        content="<h1>Hello, I'm Kenny Krosky. I'm a Software Engineer and UX Designer.</h1><p>I'm a Type 1 diabetic, family man (2 kids and 1 on the way), and a big time coffee sipper. I'm passionate about all things Front-end, specifically JavaScript, CSS, and API related. Currently employed by Adobe, leading and maintaining the Creative Cloud growth web domain.</p>"
         cta={true}
         cta_link="/letstalk"
         cta_text="👋&nbsp;&nbsp;Let's connect"
