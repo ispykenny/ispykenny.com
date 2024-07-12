2e5ca20a7bc752e1b85a9bb354ffca600764cca5
Kenny Krosky <kenny.krosky@gmail.com>
2024-07-12 14:41:17 -0700

    removed unneeded switch statement

--- providers/ThemeProvider.tsx
+++ providers/ThemeProvider.tsx
@@ -1,18 +1,18 @@
-"use client";
+'use client';
 
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/navigation';
 /* eslint-disable react-hooks/exhaustive-deps */
 
 import {
-  ReactNode,
-  createContext,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
+	ReactNode,
+	createContext,
+	useContext,
+	useEffect,
+	useState,
+} from 'react';
 
-type KeyTypes = "theme" | "isMenuOpen";
-type ValueTypes = "light" | "dark" | boolean;
+type KeyTypes = 'theme' | 'isMenuOpen';
+type ValueTypes = 'light' | 'dark' | boolean;
 
 const GetThemeSettings = createContext<any>(null);
 const SetThemeSettings = createContext<any>(null);
@@ -21,40 +21,35 @@ export const useGetThemeSettings = () => useContext(GetThemeSettings);
 export const useSetThemeSettings = () => useContext(SetThemeSettings);
 
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [settings, setSettings] = useState({});
-  const navigation = useRouter();
+	const [settings, setSettings] = useState({});
+	const navigation = useRouter();
 
-  useEffect(() => {
-    setSettings({ ...settings, isMenuOpen: false });
-  }, [navigation]);
+	useEffect(() => {
+		setSettings({ ...settings, isMenuOpen: false });
+	}, [navigation]);
 
-  useEffect(() => {
-    if (window) {
-      const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
-      if (prefersDarkMode.matches) {
-        setSettings({ theme: "dark", isMenuOpen: false });
-      } else {
-        setSettings({ theme: "light", isMenuOpen: false });
-      }
-    }
-  }, []);
+	useEffect(() => {
+		if (window) {
+			const prefersDarkMode = window.matchMedia(
+				'(prefers-color-scheme: dark)'
+			);
+			if (prefersDarkMode.matches) {
+				setSettings({ theme: 'dark', isMenuOpen: false });
+			} else {
+				setSettings({ theme: 'light', isMenuOpen: false });
+			}
+		}
+	}, []);
 
-  const changeTheme = (key: KeyTypes, value: ValueTypes) => {
-    switch (key) {
-      case "theme":
-        setSettings({ ...settings, theme: value });
-        break;
-      case "isMenuOpen":
-        setSettings({ ...settings, isMenuOpen: value });
-        break;
-    }
-  };
+	const changeTheme = (key: KeyTypes, value: ValueTypes) => {
+		setSettings({ ...settings, [key]: value });
+	};
 
-  return (
-    <GetThemeSettings.Provider value={settings}>
-      <SetThemeSettings.Provider value={changeTheme}>
-        {children}
-      </SetThemeSettings.Provider>
-    </GetThemeSettings.Provider>
-  );
+	return (
+		<GetThemeSettings.Provider value={settings}>
+			<SetThemeSettings.Provider value={changeTheme}>
+				{children}
+			</SetThemeSettings.Provider>
+		</GetThemeSettings.Provider>
+	);
 };
